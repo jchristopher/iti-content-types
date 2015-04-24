4010496428caea4cb5617770563af93b121210ee
Jonathan Christopher <jonathan@mondaybynoon.com>
2015-04-24 08:27:41 -0400

    Add Taxonomy Single Term as submodule

---
+++ iti-content-types.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: ITI Content Types
+Plugin URI: https://github.com/jchristopher/iti-content-types
+Description: Custom content types for your website. DO NOT DEACTIVATE.
+Version: 1.0.3
+Author: Jonathan Christopher
+Author URI: http://mondaybynoon.com/
+Text Domain: iti-content-types
+*/
